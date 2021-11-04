f6e7ea3449dbdd05ac2efbcb0ba37a41d2fe0676
francesco1905 <75421713+francesco1905@users.noreply.github.com>
2021-11-04 22:39:18 +0100

    Add files via upload

---
+++ calcolo combinatorio.py
@@ -0,0 +1,134 @@
+class calcComb():
+
+    def __init__(self, stringa):
+
+        self.__N = len(stringa)
+        self.__stringa = stringa
+        self.__listStringa = list(stringa)
+
+    def get_stringa(self):
+        return self.__stringa
+
+    def get_listStringa(self):
+        return self.__listStringa
+
+    def setStringa(self):
+        '''
+        modificare questo metodo in modo da verificare la coerenza delle variabili di
+        istanza presenti
+        '''
+        return 0
+
+    def charRipetuti(self):
+        '''
+        questo metodo deve creare un dictionary all'interno del quale la chiave deve essere
+        il singolo carattere, il valore deve essere il numero di ripetizioni di quel carattere
+        
+        esempi di dictionary sono presenti nel file elementi_base/dictionary.py
+        '''
+
+    def confUtil(self):
+        '''
+       str= "pop"  # nel caso di ruzzle, str deve contenere l'attributo di istanza
+it = 'words.italian.txt' # è possibile aggiungere tante variabili quanti file di lingua si posseggono
+
+f = open(it, 'r')
+line = f.readline()
+
+for line in f:  # per ogni riga del file vengono eseguite le righe di codice che seguono
+#    print(str) 
+    if str == line[:-1]:  #bisogna eliminare l'ultimo carattere dalla parola contenuta nella riga del file
+        print("vero")
+        '''
+        pass
+
+    def fattoriale(n):
+        '''
+        implementare una qualunque versione della funzione fattoriale
+        '''
+
+    def coeffBinom(n, k):
+        ''' 
+       return N fattoriale
+        '''
+        pass
+
+    # PERMUTAZIONI
+
+    def nPermutSenzaRip(self):
+        '''
+        restituire il numero di permutazioni SENZA ripetizione
+        '''
+        return 0
+
+    def nPermutConRip(self):
+        '''
+        restituire il numero di permutazioni CON ripetizione
+        '''
+        return 0
+
+    def permutSenzaRip(self):
+        '''
+        generare e restituire la lista di permutazioni SENZA ripetizione
+        '''
+        return 0
+
+    def permutConRip(self):
+        '''
+        generare e restituire la lista di permutazioni CON ripetizione
+        '''
+        return 0
+
+    # DISPOSIZIONI
+
+    def nDispSemplSenzaRip(self):
+        '''
+        restituire il numero di disposizioni semplici SENZA ripetizione
+        '''
+        return 0
+
+    def nDispSemplConRip(self):
+        '''
+        restituire il numero di disposizioni semplici CON ripetizione
+        '''
+        return 0
+
+    def dispSemplSenzaRip(self):
+        '''
+        generare e restituire la lista delle disposizioni semplici SENZA ripetizione
+        '''
+        return 0
+
+
+    def dispSemplConRip(self):
+        '''
+        generare e restituire la lista delle disposizioni semplici CON ripetizione
+        '''
+        return 0
+
+    # COMBINAZIONI
+
+    def nCombSemplSenzaRip(self):
+        '''
+        restituire il numero delle combinazioni SENZA ripetizione
+        '''
+        return 0
+
+    def nCombSemplConRip(self):
+        '''
+        restituire il numero delle combinazioni CON ripetizione
+        '''
+        return 0
+
+    def combSenzaRip(self):
+        '''
+        generare e restituire la lista delle combinazioni SENZA ripetizione
+        '''
+        return 0
+
+
+    def combConRip(self):
+        '''
+        generare e restituire la lista delle combinazioni CON ripetizione
+        '''
+        return 0
